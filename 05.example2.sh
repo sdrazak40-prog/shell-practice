895297214e9e9d9d8d53411032e2cc450fa720c6
syed <sdrazak40@gmail.com>
2025-11-15 09:44:24 +0530

    variables

--- 05.example2.sh
+++ 05.example2.sh
@@ -1,5 +1,5 @@
 #!bin/bash 
 echo "please enter your  PIN NUMBER"
-read PIN_NUMBER #GERE PIN NUMBER IS VARIABLE
+read -s PIN_NUMBER #GERE PIN NUMBER IS VARIABLE
 
 echo "pin enterd is :" $PIN_NUMBER
