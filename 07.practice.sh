ee033203e4a4ec16c9b681167bad130df4cab8a7
syed <sdrazak40@gmail.com>
2025-11-15 09:18:33 +0530

    variables

---
+++ 07.practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+# Simulated conversation between Sai and Raju
+
+echo "$PERSON1: Hey $PERSON2, did you push the latest script to Git?"
+sleep 2
+echo "$PERSON2: Not yet, $PERSON1. I’m still testing it on Windows."
+sleep 2
+echo "$PERSON1: Make sure to handle the line endings. Use dos2unix if needed."
+sleep 2
+echo "$PERSON2: Good point! I’ll add that to the pre-commit hook $PERSON1."
+
